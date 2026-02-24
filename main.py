717e001359649657c3fb3fa55bdd85b95510be15
Konstantin Wohllaub <konstantin.wohllaub@gmail.com>
2026-02-24 20:10:03 +0100

    small changes

--- main.py
+++ main.py
@@ -11,23 +11,6 @@ cookies = {
     'IS24VisitId': 'vid217f62bd-6a5b-4ca6-8a1b-bd4bf93c9251',
 }
 
-#headers = {
-#    'accept': 'application/json',
-#    'x-is24-feature': 'adKeysAndStringValues,virtualTour,referencePriceV3,verificationArray,unpublished,listFirstListing,xxlListingType,immersiveSearch,modernizationCalculator,presale',
-#    'x-is24-device': 'iphone',
-#    'priority': 'u=3',
-#    'x-emb-st': '1769259013083',
-#    'accept-language': 'de-de',
-#    'user-agent': 'ImmoScout_27.11_26.2_._',
-#    'x-emb-id': '0526B033-7C95-4CB9-B788-94E898CFEDF1',
-#    # 'cookie': 'IS24VisitId=vid217f62bd-6a5b-4ca6-8a1b-bd4bf93c9251',
-#}
-#
-#params = {
-#    'searchId': '7a29154a-9d4b-4719-8fa4-4e649eff4947',
-#    'referrer': 'resultlist',
-#}
-
 
 if __name__ == '__main__':
     currentPage = 1
