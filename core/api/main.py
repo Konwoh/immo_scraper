e4b9e5a6a8d735c4542728248457e6a13bb44aef
Konstantin Wohllaub <konstantin.wohllaub@gmail.com>
2026-04-17 23:39:42 +0200

    docker container for api

--- core/api/main.py
+++ core/api/main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
-from scraper.base import start_scraper
-from crawler.base import start_crawler
 from sqlalchemy.dialects.postgresql import insert
-from database.models import engine, Jobs, Status
+from database.models import engine, Job, Status
 from sqlalchemy.orm import Session
 
 app = FastAPI()
@@ -10,7 +8,7 @@ app = FastAPI()
 @app.get("/start_scraper")
 def start_scraper_endpoint():
     with Session(engine) as session:
-        stmt = insert(Jobs).values(job_type="scraper", status=Status.open)
+        stmt = insert(Job).values(job_type="scraper", status=Status.open)
         session.execute(stmt)
         session.commit()
         
@@ -18,6 +16,6 @@ def start_scraper_endpoint():
 @app.get("/start_crawler")
 def start_crawler_endpoint():
     with Session(engine) as session:
-        stmt = insert(Jobs).values(job_type="crawler", status=Status.open)
+        stmt = insert(Job).values(job_type="crawler", status=Status.open)
         session.execute(stmt)
         session.commit()
