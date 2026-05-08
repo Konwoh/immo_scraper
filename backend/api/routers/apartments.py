70969b429a386d262687e9a2e4e278cbc5d4ba78
Konstantin Wohllaub <konstantin.wohllaub@gmail.com>
2026-05-08 17:58:43 +0200

    added current_user functionality to api endpoints

--- backend/api/routers/apartments.py
+++ backend/api/routers/apartments.py
@@ -1,6 +1,7 @@
 from fastapi import status, HTTPException, Depends, Path, APIRouter
-from backend.database.models import Apartment, get_db
+from backend.database.models import Apartment, get_db, SearchParams, SearchResults
 from sqlalchemy.orm import Session
+from backend.api.auth.oauth2 import get_current_user
 
 router = APIRouter(
     prefix="/apartments",
@@ -8,15 +9,33 @@ router = APIRouter(
 )
 
 @router.get("/", status_code=status.HTTP_200_OK)
-def get_apartmens(db: Session = Depends(get_db)):
-    apartments = db.query(Apartment).all()
-    if apartments is not None:
+def get_apartmens(db: Session = Depends(get_db), current_user = Depends(get_current_user)):
+    apartment_ids = (
+        db.query(SearchResults.apartment_id)
+        .join(SearchParams, SearchResults.search_params_id == SearchParams.id)
+        .filter(SearchParams.user_id == current_user.id)
+    )
+
+    apartments = db.query(Apartment).filter(Apartment.id.in_(apartment_ids)).all()
+    
+    if apartments:
         return apartments
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No apartments found") 
 
 @router.get("/{apartment_id}", status_code=status.HTTP_200_OK)
-def get_apartment_by_id(db: Session = Depends(get_db), apartment_id: int = Path(gt=0)):
-    apartment = db.query(Apartment).filter(Apartment.id == apartment_id).first()
-    if apartment is not None:
-        return apartment
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"apartment with id {apartment_id} not found") 
+def get_apartment_by_id(db: Session = Depends(get_db), apartment_id: int = Path(gt=0), current_user = Depends(get_current_user)):
+    allowed_house_ids = (
+        db.query(SearchResults.apartment_id).join(SearchParams, SearchResults.search_params_id == SearchParams.id).filter(SearchParams.user_id == current_user.id).all()
+    )
+
+    allowed_house_ids = [id[0] for id in allowed_house_ids]
+
+    apartment = (db.query(Apartment).filter(Apartment.id == apartment_id).first())
+
+    if apartment is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"House with id {apartment_id} not found")
+
+    if apartment.id not in allowed_house_ids:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to retrieve this house")
+    
+    return apartment
--- backend/api/routers/houses.py
+++ backend/api/routers/houses.py
@@ -1,6 +1,8 @@
 from fastapi import status, HTTPException, Depends, Path, APIRouter
-from backend.database.models import House, get_db
+from backend.database.models import House, get_db, SearchParams, SearchResults
 from sqlalchemy.orm import Session
+from backend.api.auth.oauth2 import get_current_user
+from sqlalchemy import select
 
 router = APIRouter(
     prefix="/houses",
@@ -8,15 +10,34 @@ router = APIRouter(
 )
 
 @router.get("/", status_code=status.HTTP_200_OK)
-def get_houses(db: Session = Depends(get_db)):
-    houses = db.query(House).all()
+def get_houses(db: Session = Depends(get_db), current_user = Depends(get_current_user)):
+    house_ids = (
+        db.query(SearchResults.house_id)
+        .join(SearchParams, SearchResults.search_params_id == SearchParams.id)
+        .filter(SearchParams.user_id == current_user.id)
+    )
+
+    houses = db.query(House).filter(House.id.in_(house_ids)).all()
+    
     if houses is not None:
         return houses
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No houses found")
 
-@router.get("/{id}", status_code=status.HTTP_200_OK)
-def get_houses_by_id(db: Session = Depends(get_db), house_id: int = Path(gt=0)):
-    house = db.query(House).filter(House.id == house_id).first()
-    if house is not None:
-        return house
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"house with id: {house_id} not found")
+@router.get("/{house_id}", status_code=status.HTTP_200_OK)
+def get_house_by_id(house_id: int = Path(gt=0), db: Session = Depends(get_db), current_user=Depends(get_current_user)):
+    
+    allowed_house_ids = (
+        db.query(SearchResults.house_id).join(SearchParams, SearchResults.search_params_id == SearchParams.id).filter(SearchParams.user_id == current_user.id).all()
+    )
+
+    allowed_house_ids = [id[0] for id in allowed_house_ids]
+
+    house = (db.query(House).filter(House.id == house_id).first())
+
+    if house is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"House with id {house_id} not found")
+
+    if house.id not in allowed_house_ids:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to retrieve this house")
+
+    return house
